4d35dd82f07bfa81ea0a814ae85123985ef000fa
Xavier Watkins <xavier.watkins@gmail.com>
2015-09-17 10:58:11 +0100

    Added travis config and speed button

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-GENOME SYNTH
+# Genome Synth
+## Playing music from the genome
+
+### The science part
+To explain this idea, I've got to go back a little and give you some background information. As you probably already know, all living organsisms have in their DNA a collection of genes. These genes are encoded into proteins, which are the active components of our cells, allowing transport, building, signaling, etc. These proteins are made up of amino acids, of which there are 20 in total. Each amino acid can be represented by a letter, so a protein sequence can be written as such:
+
+>APKYDWIAMAQYLCQAYCPMFPIKPFYQIEVGLCCDKNFLDRDEKYFEKQQEVWSSCVMY
+
+The same gene, and therefore protein, can differ between species, having a different sequence. This is mostly because species have evolved differently. Some parts of the sequence will be very similar (conserved) because they play a very intricate part in the function of the protein, while some parts will be very different. Thanks to bioinformatics tools we are able to compare these sequences, aligning these conserved regions together while highlighting dissimilar regions.
+
+>V1AR_HUMAN MRLSAGPDAGPSGNSSPWWPLATGAGNTSREAEALGEGNGPPRDVRNEELAKLEIAVLAV
+
+>V1AR_RAT    MSFPRGSQDRSVGNSSPWWPLTTEGSNGSQEAARLGEGDSPLGDVRNEELAKLEIAVLAV
+
+>V1AR_MOUSE MSFPRGSHDLPAGNSSPWWPLTTEGANSSREAAGLGEGGSPPGDVRNEELAKLEVTVLAV
+
+### The sound part
+A synthesizer is an eletronic musical instrument which generates electrical signals resulting in the generation of sounds. Typically, monophonic synthesizers use one oscillator to generate a pulsing wave as the source of the sound. When pressing a key on the keyboard, the wave will be emited at the frequency corresponding to the note pressed (e.g. A=440Hz).
+
+### Putting it together
+By using one monophonic synthesizer per species for which we have aligned sequences, we are able to have 6 separate sources of sound. Each amino acid in the sequence is attributed a musical note, according to a predefined scale. We then "read" the sequences like a musical score, each synthesizer playing the corresponding note at the given position. In some cases (conserved sequence) all, or almost all synthesizers will play the same note, whereas when the sequences diverge greatly, complex chords can be heard. The result is a haunting, and dreamy experience.
+
+### What next?
+For this first version we have chosen to align the sequences for a specific protein, the AVPR1a receptor, chosen because of its involvement in social behavious within species. The next step, through the use of public APIs is to allow the sequences for any given gene/protein to be aligned and "listened to".
+
+There are also many biophysical properties we can use to control various aspects of the synthesis, for instance using subtractive synthesis to highlight hydrophobic or hydrophilic regions.
+
+Another application could be for researchers with impaired vision, as they would be able to listen to the sequences to discover properties which are currently only available through visual tools.
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,8 @@
 'use strict';
 
-// Akebono Hapi Drum// E3    F#3    G3    B3    C4    E4    F#4    G4
-var scale = {
+
+// Akebono E3    F#3    G3    B3    C4    E4    F#4    G4
+var akebono = {
 	'G':'E3',
 	'A':'F#3',
 	'V':'G3',
@@ -25,46 +26,108 @@ var scale = {
 	'-':'E7'
 };
 
+var chromatic = {
+	'G':'C1',
+	'A':'D1',
+	'V':'E1',
+	'L':'F1',
+	'I':'G1',
+	'S':'E4',
+	'T':'F#4',
+	'C':'G4',
+	'M':'B5',
+	'P':'C5',
+	'H':'E5',
+	'R':'F#5',
+	'N':'G5',
+	'Q':'B5',
+	'E':'C6',
+	'D':'E6',
+	'F':'F#6',
+	'W':'G6',
+	'Y':'B6',
+	'K':'C7',
+	'-':'E7'
+}
+
+// Default settings
+Tone.Transport.bpm.value = 65;
+$('#speed-k').val(Tone.Transport.bpm.value);
+var scale = akebono;
+
 var colors = d3.scale.category20c().domain(Object.keys(scale));
 
 var GenomeSynth = function(){
 	this.synth = new Tone.MonoSynth();
-	var filter = new Tone.Filter(370, 'lowpass');
-	this.synth.connect(filter);
-
-	var delay = new Tone.FeedbackDelay(0.7, 0.2);
-	filter.connect(delay);
-
-	var freeverb = new Tone.JCReverb(0.3);
-	delay.connect(freeverb);
-
-	freeverb.toMaster();
 };
 
 var synths = [];
 var alignmentArray = [];
 var organisms = [];
 
+var filter = new Tone.Filter(370, 'lowpass');
+// this.synth.connect(filter);
+
+var delay = new Tone.FeedbackDelay(0.7, 0.2);
+filter.connect(delay);
+
+var freeverb = new Tone.JCReverb(0.3);
+delay.connect(freeverb);
+
+freeverb.toMaster();
+
 $.each(aligments, function(i,d){
-	synths.push(new GenomeSynth());
+	var newSynth = new GenomeSynth();
+	newSynth.synth.connect(filter);
+	synths.push(newSynth);
 	alignmentArray.push(d.sequence.split(''));
 	organisms.push(d.organism);
 });
 
 var position = 0;
-Tone.Transport.setInterval(function(time){
+
+var looper = function(time){
 	$.each(synths, function(i, genomeSynth){
-		$('#'+organisms[i].split(' ')[0].toLowerCase()+'-seq').text(alignmentArray[i][position]);
-		$('#'+organisms[i].split(' ')[0].toLowerCase()+'-seq').parent().css('background-color',colors(scale[alignmentArray[i][position]]));
-		genomeSynth.synth.triggerAttackRelease(scale[alignmentArray[i][position]], '2s', time);
+		$('div #'+organisms[i].split(' ')[0].toLowerCase()+'-seq').text(alignmentArray[i][position]);
+
+		d3.select('#'+organisms[i].split(' ')[0].toLowerCase()+'-seq')
+			.transition()
+			.duration(60000/Tone.Transport.bpm.value)
+			.style('background-color',colors(scale[alignmentArray[i][position]]));
+
+		genomeSynth.synth.triggerAttackRelease(scale[alignmentArray[i][position]], '1n', time);
 	});
 	position++;
-}, '5s');
+}
 
-$('#start-button').click(function(){
-	Tone.Transport.start();
+Tone.Transport.setInterval(looper, "4n");
+
+var playing = true;
+var togglePlayState = function() {
+	if(playing){
+		Tone.Transport.stop();
+		$('#button').text('Play');
+		playing = false;
+	} else {
+		Tone.Transport.start();
+		$('#button').text('Stop');
+		playing = true;
+	}
+}
+
+$('#button').click(function(){
+	togglePlayState();
 });
 
-$('#stop-button').click(function(){
-	Tone.Transport.stop();
+// Speed knob
+$(".dial").knob({
+	'min':50,
+    'max':500,
+    'width':100,
+    'change':function(v) {
+    	Tone.Transport.bpm.value = v;
+    }
 });
+
+
+Tone.Transport.start();
